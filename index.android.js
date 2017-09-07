fba0f39f15a7dffe0201a10193483480e10a1ecc
Taylor Gaines <taylorcgaines@gmail.com>
2017-09-07 11:48:34 -0500

    Added in concurrently, can now start whole process with yarn start.

--- index.android.js
+++ index.android.js
@@ -1,3 +1,7 @@
-// android.js
-
-import "./build/src/app.js"
+import React from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+import App from './build/app'
+const rntypescript = () => <App />
+AppRegistry.registerComponent('rntypescript', () => rntypescript);
